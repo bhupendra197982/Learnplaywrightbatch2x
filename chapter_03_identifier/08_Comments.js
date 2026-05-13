c01f900e62ce274d0ae099c0fed861f0f40d0e9f
bhupendra.verma <bhupendra.verma197982@gmail.com>
2026-05-13 07:18:49 +0530

    Remove duplicate Learnplaywrightbatch2x folder and add Chapter_04_Javascript_Concepts

---
+++ chapter_03_identifier/08_Comments.js
@@ -0,0 +1,10 @@
+//This is single line comment in JavaScript
+/*
+This is a multi-line comment in JavaScript
+It can span multiple lines until the closing */
+
+/**
+ * This is a JSDoc comment in JavaScript
+ * It is used to document code, functions, classes, etc.
+ * It can include tags like @param, @returns, @example, etc.    
+ */
