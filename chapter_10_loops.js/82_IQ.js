de0dba322aec2ed4a057999ea443e1562bac42b9
bhupendra.verma <bhupendra.verma197982@gmail.com>
2026-05-22 13:17:24 +0530

    Initial commit

---
+++ chapter_10_loops.js/82_IQ.js
@@ -0,0 +1,10 @@
+// let count = 10;
+// while (count < 5) {
+//     console.log(count);
+//     count++;
+// }
+
+let n = 1;
+do {
+    console.log(n);
+} while (n < 3); // it's infinite loop because n is not changing inside the loop, so it will always be less than 3
